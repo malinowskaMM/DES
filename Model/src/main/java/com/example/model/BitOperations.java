4ddfb556d9e17b7b62231313f3e2b83cb88214c3
Magdalena Malinowska <mmalinowska.magdalena@wp.pl>
2022-05-18 20:00:36 +0200

    clean

--- Model/src/main/java/com/example/model/BitOperations.java
+++ Model/src/main/java/com/example/model/BitOperations.java
@@ -18,21 +18,4 @@ public class BitOperations {
 
         return block;
     }
-
-    public static byte[][] splitInHalf(byte[] block, int size) {
-        int splitIndex = size / 2;
-        byte[] left = new byte[splitIndex];
-        byte[] right = new byte[splitIndex];
-        for(int i = 0; i < splitIndex; i++) {
-            left[i]= block[i];
-            right[i]=block[splitIndex+i];
-        }
-        return new byte[][] {left, right};
-    }
-
-    public static byte[] shiftLeft(byte[] b1, int bytes) {
-        byte[] result = new byte[b1.length];
-        System.arraycopy(b1, bytes, result, 0, bytes);
-        return result;
-    }
 }
